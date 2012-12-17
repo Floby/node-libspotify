34a266a54ce3f54cb95b319c89831ede0ebe45ce
Florent Jaby <florent.jaby@gmail.com>
2012-12-17 14:43:19 +0100

    can login

--- src/session.h
+++ src/session.h
@@ -21,22 +21,39 @@
 #define NSP_SESSION_H
 #include <node.h>
 #include <libspotify/api.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 namespace nsp {
 
     class Session :public node::ObjectWrap {
     public:
         static void Init(v8::Handle<v8::Object> target);
+        unsigned int number;
+        void processEvents();
     
     private:
         Session ();
         virtual ~Session ();
+        void initAppKey();
+        void initConfig();
+        void initCallbacks();
+
+
+        static void onLoggedIn(sp_session* spsession, sp_error error);
+        static void onMainThreadNotified(sp_session* spsession);
 
         static v8::Handle<v8::Value> New(const v8::Arguments& args);
 
-        sp_session_config config;
-        sp_session_callbacks callbacks;
+        sp_session_config spconfig;
+        sp_session_callbacks spcallbacks;
         sp_session* spsession;
+
+        uv_timer_t process_events_handle;
+        uv_timer_t timer_handle;
+
+        uint8_t* g_appkey;
+        size_t g_appkey_size;
     };
 
 }
