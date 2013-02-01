b2aa2aa4a2c77e8e89fc77847b5c6c94174dff9f
Florent Jaby <florent.jaby@gmail.com>
2013-02-01 14:33:23 +0100

    add spotify key to travis config

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo $SPPASS
-
 whoami
 groups
 uname -a
@@ -10,6 +8,7 @@ lsb_release --all
 $base_dir = `pwd`
 
 
+# fetching libspotify linux 64bits because that's what travis runs on
 cd /opt
 sudo wget https://developer.spotify.com/download/libspotify/libspotify-12.1.51-Linux-x86_64-release.tar.gz
 sudo tar xzf libspotify-12.1.51-Linux-x86_64-release.tar.gz
@@ -18,3 +17,8 @@ sudo make install
 
 
 cd $base_dir
+mkdir spotify_key
+echo "exports.login = '$LOGIN'" > spotify_key/passwd.js
+echo "exports.password = '$PASSWORD'" >> spotify_key/passwd.js
+cd spotify_key
+wget $SPKEY
