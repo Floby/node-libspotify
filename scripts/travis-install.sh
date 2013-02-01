3fa4b668920c119593a33d89dfa250084f54677f
Florent Jaby <florent.jaby@gmail.com>
2013-02-01 14:37:58 +0100

    test travis build

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -20,5 +20,7 @@ cd $base_dir
 mkdir spotify_key
 echo "exports.login = '$LOGIN'" > spotify_key/passwd.js
 echo "exports.password = '$PASSWORD'" >> spotify_key/passwd.js
+pwd
 cd spotify_key
+pwd
 wget $SPKEY
