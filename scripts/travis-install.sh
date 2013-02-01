a54908e1d37637a559068cfd28a665d4497c4c93
Florent Jaby <florent.jaby@gmail.com>
2013-02-01 13:34:18 +0100

    test travis build

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -11,10 +11,10 @@ $base_dir = `pwd`
 
 
 cd /opt
-wget https://developer.spotify.com/download/libspotify/libspotify-12.1.51-Linux-x86_64-release.tar.gz
-tar xzf libspotify-12.1.51-Linux-x86_64-release.tar.gz
-cd libspotify-12.1.51-Linux-x86_64-release
-make install
+sudo wget https://developer.spotify.com/download/libspotify/libspotify-12.1.51-Linux-x86_64-release.tar.gz
+sudo tar xzf libspotify-12.1.51-Linux-x86_64-release.tar.gz
+sudo cd libspotify-12.1.51-Linux-x86_64-release
+sudo make install
 
 
 cd $base_dir
